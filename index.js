7852f9e70fe9aa7816a8039c802278fbd85337e2
Alexander G. Arias <Alexanderarias2@hotmail.com>
2024-10-23 19:18:19 -0700

    Implemented Mobile Device Integration.

---
+++ index.js
@@ -0,0 +1,12 @@
+// This Java script file expands the hamburger menu, and turns it to an x when expanded
+// This functionality toggles back to its original state when touched. 
+
+function expandHamburgerMenu() {
+    var navBarElements = document.querySelectorAll(".nav-content");
+    navBarElements.forEach(function(element) {
+        element.classList.toggle("show");
+    });
+    hamburgerMenuElement.classList.toggle("hamburger-x");
+}
+const hamburgerMenuElement = document.getElementById("hamburger-menu");
+hamburgerMenuElement.addEventListener("touchend", expandHamburgerMenu);
